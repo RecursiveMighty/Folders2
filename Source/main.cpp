598f3f0b08f53b654badd0ddecccf4f6b8a7d73c
Christopher S <78832631+RecursiveMighty@users.noreply.github.com>
2021-12-16 16:07:31 +0000

    stuff

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <experimental/filesystem>
 
 int main() {
-    std::ifstream fte;
-    fte.open("Source/test.txt", std::ios::in);
-    std::cout << "Opened?" << std::endl;
-    std::cout << fte.is_open() << std::endl;
-    if (fte.is_open()) {
-        std::cout << "It works!" << std::endl;
+    std::ifstream main_fds;
+    main_fds.open("Source/main.fds");
+
+    if (main_fds.is_open()) {
+        std::string line;
+        int cmd_num = 0;
+        while (std::getline(main_fds,line)) {
+            if (line == "newcmd") {
+                std::filesystem::create_directories("Source/Output/"+std::to_string(++cmd_num)+"/1");
+            }
+            std::cout << line << std::endl;
+        }
     }
     return 0;
 }
